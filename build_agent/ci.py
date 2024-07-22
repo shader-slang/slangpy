5f85825122b27352e7b797f68fbf64311a03978a
Chris Cummings <ccummings@nvidia.com>
2024-07-22 15:17:25 +0100

    Correctly disable linux + add some code to ci.py

--- build_agent/ci.py
+++ build_agent/ci.py
@@ -1,1 +1,19 @@
-# Python CI script
+import argparse
+import os
+import subprocess
+import sys
+
+
+# Helper to run a command.
+def run_command(command, shell=True):
+    print(command)
+    sys.stdout.flush()
+    result = subprocess.run(command, shell=shell)
+    if result.returncode != 0:
+        raise (f"Error running command: {command}")
+    return result
+
+
+run_command("pip install --editable .")
+
+run_command("pytest tests")
