1d757ccc9237b89463ed0984826849a95640e6c9
Chris Cummings <ccummings@nvidia.com>
2024-08-19 08:27:32 -0700

    Add dummy copper folder

--- .build_agent/ci.py
+++ .build_agent/ci.py
@@ -19,14 +19,14 @@ run_command(
     "pip install --upgrade --force-reinstall git+https://gitlab-master.nvidia.com/skallweit/sgl.git"
 )
 
+# run precommit
+run_command("pre-commit run --all-files")
+
 # install this package as editable
 run_command("pip install --editable .")
 
 # install dev requirements
 run_command("pip install -r requirements-dev.txt")
 
-# run precommit
-run_command("pre-commit run --all-files")
-
 # run tests
-run_command("pytest --junit-xml=junit-test.xml tests")
+run_command("pytest --junit-xml=junit-test.xml")
