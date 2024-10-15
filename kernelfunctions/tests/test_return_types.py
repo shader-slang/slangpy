5f8c922fbc4bf6db7ec088adf17d7a1e1121101c
Benedikt Bitterli <bbitterli@nvidia.com>
2024-10-15 14:45:31 -0700

    Add custom return type tests

---
+++ kernelfunctions/tests/test_return_types.py
@@ -0,0 +1,62 @@
+from typing import Any
+import pytest
+from kernelfunctions.backend import DeviceType, TypeReflection
+from kernelfunctions.backend.slangpynativeemulation import CallContext
+from kernelfunctions.core.boundvariableruntime import BoundVariableRuntime
+import kernelfunctions.tests.helpers as helpers
+import kernelfunctions.typeregistry as tr
+from kernelfunctions.bindings.valuereftype import ValueRefType
+from kernelfunctions.core import ReturnContext
+
+
+class Test:
+    def __init__(self, x: int):
+        self.x = x
+
+
+class TestType(ValueRefType):
+    def __init__(self):
+        super().__init__(tr.SLANG_SCALAR_TYPES[TypeReflection.ScalarType.int32])
+
+    def read_output(self, context: CallContext, binding: BoundVariableRuntime, data: Any):
+        return Test(super().read_output(context, binding, data))
+
+
+def create_test_type(value: Any):
+    if isinstance(value, Test):
+        return TestType()
+    elif isinstance(value, ReturnContext):
+        if value.slang_type.name != "int":
+            return None
+        if value.bind_context.call_dimensionality != 0:
+            return None
+        return TestType()
+    else:
+        return None
+
+
+tr.PYTHON_TYPES[Test] = create_test_type
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_returnvalue(device_type: DeviceType):
+
+    device = helpers.get_device(device_type)
+    function = helpers.create_function_from_module(
+        device,
+        "add_numbers",
+        r"""
+int add_numbers(int a, int b) {
+    return a+b;
+}
+""",
+    )
+
+    res = function.return_type(Test).call(4, 5)
+
+    assert isinstance(res, Test)
+    assert res.x == 9
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v"])
