db9344e2c974cf4365c5e2fa4f8b20ba7915d677
Chris Cummings <ccummings@nvidia.com>
2024-10-21 16:45:39 +0100

    First full implementation of texture types with tests

--- kernelfunctions/bindings/texturetype.py
+++ kernelfunctions/bindings/texturetype.py
@@ -1,11 +1,14 @@
 
 
-from typing import Optional
+from typing import Any, Optional
 
-from kernelfunctions.core import BaseType, Shape
+from kernelfunctions.backend.slangpynativeemulation import CallContext
+from kernelfunctions.core import BaseType, Shape, AccessType, BindContext, BoundVariable, CodeGenBlock
 
-from kernelfunctions.backend import Texture, TypeReflection
-from kernelfunctions.typeregistry import SLANG_STRUCT_TYPES_BY_NAME, get_or_create_type
+from kernelfunctions.backend import Texture, TypeReflection, ResourceUsage, ResourceType, get_format_info, FormatType
+
+from kernelfunctions.core.boundvariableruntime import BoundVariableRuntime
+from kernelfunctions.typeregistry import PYTHON_SIGNATURES, PYTHON_TYPES, SLANG_STRUCT_TYPES_BY_NAME, SLANG_VECTOR_TYPES, get_or_create_type
 
 from .valuetype import ValueType
 
@@ -20,12 +23,78 @@ class TextureType(ValueType):
         self.element_type = element_type
         self.name = f"{self._prefix()}{self._base_texture_type_name}<{self.element_type.name}>"
 
+    def is_writable(self):
+        return self._writable
+
     def _prefix(self):
         return "RW" if self._writable else ""
+
+    def build_accessor_name(self, writable: Optional[bool] = None):
+        if writable is None:
+            writable = self._writable
+        assert self.element_type is not None
+        return f"{self._prefix()}{self._base_texture_type_name}Type<{self.element_type.name}>"
+
+    # Call data can only be read access to primal, and simply declares it as a variable
+    def gen_calldata(self, cgb: CodeGenBlock, context: BindContext, binding: 'BoundVariable'):
+        assert self.element_type is not None
+        access = binding.access
+        name = binding.variable_name
+
+        if binding.call_dimensionality == 0:
+            # If broadcast directly, function is just taking the texture argument directly, so use the slang type
+            assert not access[0] in [AccessType.write, AccessType.readwrite]
+            assert isinstance(binding.slang.primal, TextureType)
+            cgb.type_alias(
+                f"_{name}", binding.slang.primal.build_accessor_name())
+        elif binding.call_dimensionality == self._texture_shape:
+            # If broadcast is the same shape as the texture, this is loading from pixels, so use the
+            # type required to support the required access
+            if not self._writable and access[0] in [AccessType.write, AccessType.readwrite]:
+                raise ValueError(f"Cannot write to read-only texture {name}")
+            if access[0] in [AccessType.write, AccessType.readwrite]:
+                cgb.type_alias(
+                    f"_{name}", self.build_accessor_name(True))
+            elif access[0] == AccessType.read:
+                cgb.type_alias(
+                    f"_{name}", self.build_accessor_name(False))
+            else:
+                cgb.type_alias(f"_{name}", f"NoneType")
+        else:
+            raise ValueError(
+                f"Texture {name} has invalid transform {binding.transform}")
+
+    # Call data just returns the primal
+    def create_calldata(self, context: CallContext, binding: 'BoundVariableRuntime', data: Any) -> Any:
+        access = binding.access
+        if access[0] != AccessType.none:
+            return {
+                'value': data
+            }
 
     @property
     def differentiable(self):
         return self.element_type.differentiable
+
+
+class Texture1DType(TextureType):
+    def __init__(self, element_type: BaseType, writable: bool):
+        super().__init__(element_type=element_type, writable=writable,
+                         base_texture_type_name="Texture1D", texture_shape=1)
+
+    def get_container_shape(self, value: Optional[Texture] = None) -> Shape:
+        if value is not None:
+            return Shape(value.width)
+        else:
+            return Shape(-1)
+
+    @property
+    def derivative(self):
+        el_diff = self.element_type.derivative
+        if el_diff is not None:
+            return Texture1DType(el_diff, self._writable)
+        else:
+            return None
 
 
 class Texture2DType(TextureType):
@@ -48,15 +117,170 @@ class Texture2DType(TextureType):
             return None
 
 
+class Texture1DArrayType(TextureType):
+    def __init__(self, element_type: BaseType, writable: bool):
+        super().__init__(element_type=element_type, writable=writable,
+                         base_texture_type_name="Texture1DArray", texture_shape=2)
+
+    def get_container_shape(self, value: Optional[Texture] = None) -> Shape:
+        if value is not None:
+            return Shape(value.array_size, value.width)
+        else:
+            return Shape(-1, -1)
+
+    @property
+    def derivative(self):
+        el_diff = self.element_type.derivative
+        if el_diff is not None:
+            return Texture1DType(el_diff, self._writable)
+        else:
+            return None
+
+
+class Texture2DArrayType(TextureType):
+    def __init__(self, element_type: BaseType, writable: bool):
+        super().__init__(element_type=element_type, writable=writable,
+                         base_texture_type_name="Texture2DArray", texture_shape=3)
+
+    def get_container_shape(self, value: Optional[Texture] = None) -> Shape:
+        if value is not None:
+            return Shape(value.array_size, value.width, value.height)
+        else:
+            return Shape(-1, -1, -1)
+
+    @property
+    def derivative(self):
+        el_diff = self.element_type.derivative
+        if el_diff is not None:
+            return Texture2DType(el_diff, self._writable)
+        else:
+            return None
+
+
+class Texture3DType(TextureType):
+    def __init__(self, element_type: BaseType, writable: bool):
+        super().__init__(element_type=element_type, writable=writable,
+                         base_texture_type_name="Texture3D", texture_shape=3)
+
+    def get_container_shape(self, value: Optional[Texture] = None) -> Shape:
+        if value is not None:
+            return Shape(value.width, value.height, value.depth)
+        else:
+            return Shape(-1, -1, -1)
+
+    @property
+    def derivative(self):
+        el_diff = self.element_type.derivative
+        if el_diff is not None:
+            return Texture3DType(el_diff, self._writable)
+        else:
+            return None
+
+
+class TextureCubeType(TextureType):
+    def __init__(self, element_type: BaseType, writable: bool):
+        super().__init__(element_type=element_type, writable=writable,
+                         base_texture_type_name="TextureCube", texture_shape=3)
+
+    def get_container_shape(self, value: Optional[Texture] = None) -> Shape:
+        if value is not None:
+            return Shape(value.width, value.height, value.depth)
+        else:
+            return Shape(-1, -1, -1)
+
+    @property
+    def derivative(self):
+        el_diff = self.element_type.derivative
+        if el_diff is not None:
+            return Texture3DType(el_diff, self._writable)
+        else:
+            return None
+
+
+class TextureCubeArrayType(TextureType):
+    def __init__(self, element_type: BaseType, writable: bool):
+        super().__init__(element_type=element_type, writable=writable,
+                         base_texture_type_name="TextureCubeArray", texture_shape=4)
+
+    def get_container_shape(self, value: Optional[Texture] = None) -> Shape:
+        if value is not None:
+            return Shape(-1, 6, value.width, value.height)
+        else:
+            return Shape(-1, 6, -1, -1)
+
+    @property
+    def derivative(self):
+        el_diff = self.element_type.derivative
+        if el_diff is not None:
+            return Texture3DType(el_diff, self._writable)
+        else:
+            return None
+
+
 def _get_or_create_slang_type_reflection(slang_type: TypeReflection) -> BaseType:
     assert isinstance(slang_type, TypeReflection)
     assert slang_type.kind == TypeReflection.Kind.resource
-    if slang_type.resource_shape.name == "texture_2d":
-        return Texture2DType(
-            element_type=get_or_create_type(slang_type.resource_result_type),
-            writable=slang_type.resource_access == TypeReflection.ResourceAccess.read_write)
+    et = get_or_create_type(slang_type.resource_result_type)
+    writable = slang_type.resource_access == TypeReflection.ResourceAccess.read_write
+    if slang_type.resource_shape == TypeReflection.ResourceShape.texture_1d:
+        return Texture1DType(element_type=et, writable=writable)
+    elif slang_type.resource_shape == TypeReflection.ResourceShape.texture_2d:
+        return Texture2DType(element_type=et, writable=writable)
+    elif slang_type.resource_shape == TypeReflection.ResourceShape.texture_3d:
+        return Texture3DType(element_type=et, writable=writable)
+    elif slang_type.resource_shape == TypeReflection.ResourceShape.texture_cube:
+        return TextureCubeType(element_type=et, writable=writable)
+    elif slang_type.resource_shape == TypeReflection.ResourceShape.texture_1d_array:
+        return Texture1DArrayType(element_type=et, writable=writable)
+    elif slang_type.resource_shape == TypeReflection.ResourceShape.texture_2d_array:
+        return Texture2DArrayType(element_type=et, writable=writable)
+    elif slang_type.resource_shape == TypeReflection.ResourceShape.texture_cube_array:
+        return TextureCubeArrayType(element_type=et, writable=writable)
     else:
         raise ValueError(f"Unsupported slang type {slang_type}")
 
 
 SLANG_STRUCT_TYPES_BY_NAME['__TextureImpl'] = _get_or_create_slang_type_reflection
+SLANG_STRUCT_TYPES_BY_NAME['_Texture'] = _get_or_create_slang_type_reflection
+
+
+def _get_or_create_python_type(value: Texture):
+    assert isinstance(value, Texture)
+    usage = value.desc.usage
+    writable = (usage & ResourceUsage.unordered_access.value) != 0
+
+    fmt_info = get_format_info(value.desc.format)
+    if fmt_info.type in [FormatType.float, FormatType.unorm, FormatType.snorm, FormatType.unorm_srgb]:
+        scalar_type = TypeReflection.ScalarType.float32
+    elif fmt_info.type == FormatType.uint:
+        scalar_type = TypeReflection.ScalarType.uint32
+    elif fmt_info.type == FormatType.sint:
+        scalar_type = TypeReflection.ScalarType.int32
+    else:
+        raise ValueError(f"Unsupported format {value.desc.format}")
+    element_type = SLANG_VECTOR_TYPES[scalar_type][fmt_info.channel_count]
+
+    if value.array_size == 1:
+        if value.desc.type == ResourceType.texture_1d:
+            return Texture1DType(element_type, writable)
+        elif value.desc.type == ResourceType.texture_2d:
+            return Texture2DType(element_type, writable)
+        elif value.desc.type == ResourceType.texture_3d:
+            return Texture3DType(element_type, writable)
+        elif value.desc.type == ResourceType.texture_cube:
+            return TextureCubeType(element_type, writable)
+        else:
+            raise ValueError(f"Unsupported texture type {value.desc.type}")
+    else:
+        if value.desc.type == ResourceType.texture_1d:
+            return Texture1DArrayType(element_type, writable)
+        elif value.desc.type == ResourceType.texture_2d:
+            return Texture2DArrayType(element_type, writable)
+        elif value.desc.type == ResourceType.texture_cube:
+            return TextureCubeArrayType(element_type, writable)
+        else:
+            raise ValueError(f"Unsupported texture type {value.desc.type}")
+
+
+PYTHON_TYPES[Texture] = _get_or_create_python_type
+PYTHON_SIGNATURES[Texture] = lambda x: f"[{x.desc.type},{x.desc.usage},{x.desc.format},{x.array_size>1}]"
---
+++ kernelfunctions/tests/test_textures.py
@@ -0,0 +1,230 @@
+import pytest
+from kernelfunctions.backend import DeviceType, Format, int2, ResourceUsage, ResourceType
+from kernelfunctions.function import Function
+from kernelfunctions.instance import InstanceListBuffer
+from kernelfunctions.module import Module
+from kernelfunctions.struct import Struct
+import kernelfunctions.tests.helpers as helpers
+import numpy as np
+
+from kernelfunctions.types.buffer import NDBuffer
+
+
+def load_test_module(device_type: DeviceType):
+    device = helpers.get_device(device_type)
+    return Module(device.load_module("test_textures.slang"))
+
+# Generate random data for a texture with a given array size and mip count.
+
+
+def make_rand_data(type: ResourceType, array_size: int, mip_count: int):
+
+    if type == ResourceType.texture_cube:
+        array_size *= 6
+        type = ResourceType.texture_2d
+
+    levels = []
+    for i in range(0, array_size):
+        sz = 32
+        mips = []
+        for i in range(0, mip_count):
+            if type == ResourceType.texture_1d:
+                mips.append(np.random.rand(sz, 4).astype(np.float32))
+            elif type == ResourceType.texture_2d:
+                mips.append(np.random.rand(sz, sz, 4).astype(np.float32))
+            elif type == ResourceType.texture_3d:
+                mips.append(np.random.rand(sz, sz, sz, 4).astype(np.float32))
+            else:
+                raise ValueError(f"Unsupported resource type: {type}")
+            sz = int(sz / 2)
+        levels.append(mips)
+    return levels
+
+
+# Generate dictionary of arguments for creating a texture.
+def make_args(type: ResourceType, array_size: int, mips: int):
+    args = {
+        "format": Format.rgba32_float,
+        "usage": ResourceUsage.shader_resource | ResourceUsage.unordered_access,
+        "mip_count": mips,
+        "array_size": array_size,
+    }
+    if type == ResourceType.texture_1d:
+        args.update({"type": type, "width": 32})
+    elif type == ResourceType.texture_2d:
+        args.update({"type": type, "width": 32, "height": 32})
+    elif type == ResourceType.texture_3d:
+        args.update({"type": type, "width": 32, "height": 32, "depth": 32})
+    elif type == ResourceType.texture_cube:
+        args.update({"type": type, "width": 32, "height": 32})
+    else:
+        raise ValueError(f"Unsupported resource type: {type}")
+    return args
+
+
+@pytest.mark.parametrize(
+    "type",
+    [
+        ResourceType.texture_1d,
+        ResourceType.texture_2d,
+        ResourceType.texture_3d,
+        ResourceType.texture_cube,
+    ],
+)
+def make_grid_data(type: ResourceType, array_size: int = 1):
+    if array_size == 1:
+        if type == ResourceType.texture_1d:
+            data = np.zeros((32, 1), dtype=np.int32)
+            for i in range(32):
+                data[i, 0] = i
+        elif type == ResourceType.texture_2d:
+            data = np.zeros((32, 32, 2), dtype=np.int32)
+            for i in range(32):
+                for j in range(32):
+                    data[i, j] = [i, j]
+        elif type == ResourceType.texture_3d:
+            data = np.zeros((32, 32, 32, 3), dtype=np.int32)
+            for i in range(32):
+                for j in range(32):
+                    for k in range(32):
+                        data[i, j, k] = [i, j, k]
+        elif type == ResourceType.texture_cube:
+            # cube
+            data = np.zeros((6, 32, 32, 3), dtype=np.int32)
+            for i in range(6):
+                for j in range(32):
+                    for k in range(32):
+                        data[i, j, k] = [i, j, k]
+        else:
+            raise ValueError("Invalid dims")
+    else:
+        if type == ResourceType.texture_1d:
+            data = np.zeros((array_size, 32, 2), dtype=np.int32)
+            for ai in range(array_size):
+                for i in range(32):
+                    data[ai, i] = [ai, i]
+        elif type == ResourceType.texture_2d:
+            data = np.zeros((array_size, 32, 32, 3), dtype=np.int32)
+            for ai in range(array_size):
+                for i in range(32):
+                    for j in range(32):
+                        data[ai, i, j] = [ai, i, j]
+        elif type == ResourceType.texture_3d:
+            data = np.zeros((array_size, 32, 32, 32, 4), dtype=np.int32)
+            for ai in range(array_size):
+                for i in range(32):
+                    for j in range(32):
+                        for k in range(32):
+                            data[ai, i, j, k] = [ai, i, j, k]
+        elif type == ResourceType.texture_cube:
+            # cube
+            data = np.zeros((array_size, 6, 32, 32, 4), dtype=np.int32)
+            for ai in range(array_size):
+                for i in range(6):
+                    for j in range(32):
+                        for k in range(32):
+                            data[ai, i, j, k] = [ai, i, j, k]
+        else:
+            raise ValueError("Invalid dims")
+
+    return data
+
+
+@pytest.mark.parametrize(
+    "type",
+    [
+        ResourceType.texture_1d,
+        ResourceType.texture_2d,
+        ResourceType.texture_3d
+    ],
+)
+@pytest.mark.parametrize("slices", [1, 4])
+@pytest.mark.parametrize("mips", [0, 1, 4])
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_read_write_texture(
+    device_type: DeviceType, slices: int, mips: int, type: ResourceType
+):
+    m = load_test_module(device_type)
+    assert m is not None
+
+    # No 3d texture arrays.
+    if type == ResourceType.texture_3d and slices > 1:
+        return
+
+    if type == ResourceType.texture_1d and slices > 1:
+        pytest.skip("Pending slang fix")
+
+    # populate a buffer of grid coordinates
+    grid_coords_data = make_grid_data(type, slices)
+    dims = len(grid_coords_data.shape) - 1
+    grid_coords = InstanceListBuffer(struct=getattr(
+        m, f"int{dims}").as_struct(), shape=grid_coords_data.shape[0:-1])
+    grid_coords.from_numpy(grid_coords_data)
+
+    # Create texture and build random data
+    src_tex = m.device.create_texture(**make_args(type, slices, mips))
+    dest_tex = m.device.create_texture(**make_args(type, slices, mips))
+    rand_data = make_rand_data(src_tex.type, src_tex.array_size, src_tex.mip_count)
+
+    # Write random data to texture
+    for slice_idx, slice_data in enumerate(rand_data):
+        for mip_idx, mip_data in enumerate(slice_data):
+            src_tex.from_numpy(mip_data, array_slice=slice_idx, mip_level=mip_idx)
+
+    array_nm = ""
+    if slices > 1:
+        array_nm = f"_array"
+
+    func = getattr(m, f"copy_pixel_{type.name}{array_nm}")
+    func(grid_coords, src_tex, dest_tex)
+
+    # Read back data and compare (currently just messing with mip 0)
+    for slice_idx, slice_data in enumerate(rand_data):
+        data = dest_tex.to_numpy(array_slice=slice_idx, mip_level=0)
+        assert np.allclose(data, rand_data[slice_idx][0])
+
+
+@pytest.mark.parametrize(
+    "type",
+    [
+        ResourceType.texture_1d,
+        ResourceType.texture_2d,
+        ResourceType.texture_3d
+    ],
+)
+@pytest.mark.parametrize("slices", [1, 4])
+@pytest.mark.parametrize("mips", [1])
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_copy_value(
+    device_type: DeviceType, slices: int, mips: int, type: ResourceType
+):
+    m = load_test_module(device_type)
+    assert m is not None
+
+    # No 3d texture arrays.
+    if type == ResourceType.texture_3d and slices > 1:
+        return
+
+    if type == ResourceType.texture_1d and slices > 1:
+        pytest.skip("Pending slang fix")
+
+    # Create texture and build random data
+    src_tex = m.device.create_texture(**make_args(type, slices, mips))
+    dest_tex = m.device.create_texture(**make_args(type, slices, mips))
+    rand_data = make_rand_data(src_tex.type, src_tex.array_size, src_tex.mip_count)
+
+    # Write random data to texture
+    for slice_idx, slice_data in enumerate(rand_data):
+        for mip_idx, mip_data in enumerate(slice_data):
+            src_tex.from_numpy(mip_data, array_slice=slice_idx, mip_level=mip_idx)
+
+    m.copy_value(src_tex, dest_tex)
+
+    # Read back data and compare (currently just messing with mip 0)
+    for slice_idx, slice_data in enumerate(rand_data):
+        data = dest_tex.to_numpy(array_slice=slice_idx, mip_level=0)
+        assert np.allclose(data, rand_data[slice_idx][0])
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v", "-s"])
