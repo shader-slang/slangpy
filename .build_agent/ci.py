1cd8a497c3f753a089a5f26a22458287881e9aaf
Chris Cummings <ccummings@nvidia.com>
2024-11-13 09:17:54 +0000

    Enable pip installation steps for ci

--- .build_agent/ci.py
+++ .build_agent/ci.py
@@ -14,28 +14,32 @@ def run_command(command: str, shell: bool = True):
     return result
 
 
-print("hello")
+FAILED = False
 
-exit(0)
+try:
+    # struggling to get sgl to install via requirements - install directly here instead
+    run_command(
+        "pip install --upgrade --force-reinstall git+https://github.com/shader-slang/sgl.git"
+    )
 
-# struggling to get sgl to install via requirements - install directly here instead
-run_command(
-    "pip install --upgrade --force-reinstall git+https://github.com/shader-slang/sgl.git"
-)
+    # install this package as editable
+    run_command("pip install --editable .")
 
-# install this package as editable
-run_command("pip install --editable .")
+    # install dev requirements
+    run_command("pip install -r requirements-dev.txt")
 
-# install dev requirements
-run_command("pip install -r requirements-dev.txt")
+    # run precommit
+    run_command("pre-commit run --all-files")
 
-# run precommit
-run_command("pre-commit run --all-files")
+    # run tests with native emulation
+    # os.environ["SLANGPY_DISABLE_NATIVE"] = "1"
+    # run_command("pytest --junit-xml=junit-test-emu.xml")
 
-# run tests with native emulation
-os.environ["SLANGPY_DISABLE_NATIVE"] = "1"
-run_command("pytest --junit-xml=junit-test-emu.xml")
+    # run tests with native
+    # del os.environ["SLANGPY_DISABLE_NATIVE"]
+    # run_command("pytest --junit-xml=junit-test.xml")
+except Exception as e:
+    print(e)
+    FAILED = True
 
-# run tests with native
-del os.environ["SLANGPY_DISABLE_NATIVE"]
-run_command("pytest --junit-xml=junit-test.xml")
+run_command("pip uninstall -y git+https://github.com/shader-slang/sgl.git")
