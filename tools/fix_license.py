b921b66890aff5b5a772113d07dccde9cc109a9c
Chris Cummings <ccummings@nvidia.com>
2024-12-04 16:49:42 +0000

    Fixup pre-commit hooks

--- tools/fix_license.py
+++ tools/fix_license.py
@@ -5,28 +5,32 @@ from pathlib import Path
 SPDX_IDENTIFIER = "# SPDX-License-Identifier: Apache-2.0"
 
 
+def process_file(file: Path):
+    if not file.name.endswith(".py"):
+        return
+
+    # read whole file as text
+    with open(file, "r") as f:
+        text = f.read()
+
+    # split text into lines
+    lines = [x for x in text.split("\n") if not x.startswith(SPDX_IDENTIFIER)]
+
+    lines.insert(0, SPDX_IDENTIFIER)
+
+    newtext = "\n".join(lines)
+
+    if newtext != text:
+        print(file)
+        with open(file, "w") as f:
+            f.write(newtext)
+
+
 def run(root: Path):
     files = list(root.glob("**/*.py"))
 
     for file in files:
-        if not file.name.endswith(".py"):
-            continue
-
-        # read whole file as text
-        with open(file, "r") as f:
-            text = f.read()
-
-        # split text into lines
-        lines = [x for x in text.split("\n") if not x.startswith(SPDX_IDENTIFIER)]
-
-        lines.insert(0, SPDX_IDENTIFIER)
-
-        newtext = "\n".join(lines)
-
-        if newtext != text:
-            print(file)
-            with open(file, "w") as f:
-                f.write(newtext)
+        process_file(file)
 
 
 if __name__ == "__main__":
