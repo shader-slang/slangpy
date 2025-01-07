c766009af0d3aada09e1d0c0e23afafcd2ff725c
Chris Cummings <ccummings@nvidia.com>
2025-01-07 10:32:19 +0000

    More docs fixes

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'SlangPy'
 copyright = '2025, Chris Cummings, Benedikt Bitterli, Sai Bangaru, Yong Hei, NVIDIA'
