e5c4241c4e2f6bb20a1a20fe4e4963f61b884413
Benedikt Bitterli <bbitterli@nvidia.com>
2025-01-08 22:16:10 -0800

    Add primal tensor tests

---
+++ slangpy/tests/test_tensor.py
@@ -0,0 +1,78 @@
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+from slangpy.backend import DeviceType, Device
+from slangpy.types import Tensor
+import slangpy.tests.helpers as helpers
+import numpy as np
+from typing import Any
+import os
+
+
+def get_test_tensors(device: Device, N: int = 4):
+    np.random.seed(0)
+
+    np_weights = np.random.randn(8, 8).astype(np.float32)
+    np_biases = np.random.randn(8).astype(np.float32)
+    np_x = np.random.randn(8).astype(np.float32)
+    np_result = np.tile(np_weights.dot(np_x) + np_biases, (N, 1))
+
+    weights = Tensor.from_numpy(np_weights, device).broadcast_to((N, 8, 8))
+    biases = Tensor.from_numpy(np_biases, device).broadcast_to((N, 8))
+    x = Tensor.from_numpy(np_x, device).broadcast_to((N, 8))
+
+    return weights, biases, x, np_result
+
+
+def get_func(device: Device, name: str):
+    path = os.path.split(__file__)[0] + "/test_tensor.slang"
+    return helpers.create_function_from_module(device, name, open(path, "r").read())
+
+
+def compare_tensors(a: np.ndarray[Any, Any], b: np.ndarray[Any, Any]):
+    assert a.shape == b.shape, f"Tensor shape {a.shape} does not match expected shape {b.shape}"
+    err = np.max(np.abs(a - b))
+    assert err < 1e-4, f"Tensor deviates by {err} from reference"
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_tensor_arguments(device_type: DeviceType):
+    device = helpers.get_device(device_type)
+
+    func = get_func(device, "matrix_vector_direct").return_type(Tensor)
+    weights, biases, x, np_result = get_test_tensors(device)
+    y = func(weights, biases, x)
+    compare_tensors(y.to_numpy(), np_result)
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_tensor_parameters(device_type: DeviceType):
+    device = helpers.get_device(device_type)
+
+    func = get_func(device, "matrix_vector_tensorized").return_type(Tensor)
+    weights, biases, x, np_result = get_test_tensors(device)
+    y = func(weights, biases, x)
+    compare_tensors(y.to_numpy(), np_result)
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_interface_parameters(device_type: DeviceType):
+    device = helpers.get_device(device_type)
+
+    func = get_func(device, "matrix_vector_interfaces").return_type(Tensor)
+    weights, biases, x, np_result = get_test_tensors(device)
+    y = func(weights, biases, x)
+    compare_tensors(y.to_numpy(), np_result)
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_generic_call(device_type: DeviceType):
+    device = helpers.get_device(device_type)
+
+    func = get_func(device, "matrix_vector_generic<8, 8>").return_type(Tensor)
+    weights, biases, x, np_result = get_test_tensors(device)
+    y = func(weights, biases, x)
+    compare_tensors(y.to_numpy(), np_result)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v", "-s"])
