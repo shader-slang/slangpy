3f1eb0dae445464d19846dd238416266f6657938
Benedikt Bitterli <bbitterli@nvidia.com>
2025-01-27 01:52:22 -0800

    Fix array marshall

--- slangpy/builtin/array.py
+++ slangpy/builtin/array.py
@@ -4,12 +4,12 @@ from typing import Any
 from slangpy.core.native import Shape
 
 import slangpy.bindings.typeregistry as tr
-from slangpy.bindings import Marshall
-from slangpy.builtin.value import ValueMarshall
+from slangpy.reflection import SlangType, ArrayType, ScalarType
+from slangpy.bindings import Marshall, BindContext, CodeGenBlock, BoundVariable
 from slangpy.reflection import SlangProgramLayout
 
 
-class ArrayMarshall(ValueMarshall):
+class ArrayMarshall(Marshall):
     def __init__(self, layout: SlangProgramLayout, element_type: Marshall, element_count: int):
         super().__init__(layout)
         self.slang_type = layout.array_type(element_type.slang_type, element_count)
@@ -17,6 +17,19 @@ class ArrayMarshall(ValueMarshall):
     def get_shape(self, value: list[Any]) -> Shape:
         slang_shape = self.slang_type.shape
         return Shape((len(value),)+slang_shape.as_tuple()[1:])
+
+    def resolve_type(self, context: BindContext, bound_type: SlangType):
+        # If we're dealing with scalars, conform to the target type. Otherwise, passing
+        # scalar types becomes quite hard - e.g. python only knows int, and trying to
+        # pass to uint/int16 etc. would always throw an error
+        if isinstance(bound_type, ArrayType) and isinstance(bound_type.element_type, ScalarType):
+            return bound_type
+
+        return self.slang_type
+
+    def gen_calldata(self, cgb: CodeGenBlock, context: BindContext, binding: BoundVariable):
+        name = binding.variable_name
+        cgb.type_alias(f"_t_{name}", f"ValueType<{binding.vector_type.full_name}>")
 
     # An array can be passed directly as a value for raw dispatch
     def create_dispatchdata(self, data: Any) -> Any:
@@ -28,7 +41,7 @@ class ArrayMarshall(ValueMarshall):
 
 
 def python_lookup_array_type(layout: SlangProgramLayout, value: list[Any]) -> Marshall:
-    et = tr.get_or_create_type(layout, value[0])
+    et = tr.get_or_create_type(layout, type(value[0]), value[0])
     return ArrayMarshall(layout, et, len(value))
 
 
