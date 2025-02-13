d317e82af414f1103e8bea1ae332033f63aac1be
Chris Cummings <ccummings@nvidia.com>
2025-02-13 14:01:17 +0000

    Output info / requirements in samples

--- examples/generators/main_grid.py
+++ examples/generators/main_grid.py
@@ -5,6 +5,8 @@ import slangpy as spy
 import pathlib
 import numpy as np
 
+print("SlangPy grid generator example (https://slangpy.shader-slang.org/en/latest/generator_grid.html)")
+
 # Create an SGL device with the local folder for slangpy includes
 device = spy.create_device(include_paths=[
     pathlib.Path(__file__).parent.absolute(),
--- examples/generators/main_ids.py
+++ examples/generators/main_ids.py
@@ -5,6 +5,8 @@ import slangpy as spy
 import pathlib
 import numpy as np
 
+print("SlangPy id generator examples (https://slangpy.shader-slang.org/en/latest/generator_ids.html)")
+
 # Create an SGL device with the local folder for slangpy includes
 device = spy.create_device(include_paths=[
     pathlib.Path(__file__).parent.absolute(),
