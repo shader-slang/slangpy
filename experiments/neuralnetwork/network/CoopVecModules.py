0f201496e597c60866a2de328530ad2c544ec56b
Benedikt Bitterli <bbitterli@nvidia.com>
2025-02-19 17:48:39 -0800

    Intermediate commit of network refactors

---
+++ experiments/neuralnetwork/network/CoopVecModules.py
@@ -0,0 +1,140 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+from __future__ import annotations
+
+from slangpy.backend import Device, CoopVecMatrixLayout
+from slangpy.types import Tensor
+from typing import Optional
+import numpy as np
+import math
+
+from .NeuralModules import ArrayModule, Real
+
+
+class CoopVecModule(ArrayModule):
+    def __init__(self, num_inputs: int, num_outputs: int, dtype: Real = Real.half):
+        super().__init__(num_inputs, num_outputs, dtype)
+
+    @property
+    def input_type(self) -> str:
+        return f"DiffCoopVec<{self.dtype}, {self.num_inputs}>"
+
+    @property
+    def output_type(self) -> str:
+        return f"DiffCoopVec<{self.dtype}, {self.num_outputs}>"
+
+
+class LinearLayer(CoopVecModule):
+    def __init__(self, num_inputs: int, num_outputs: int, dtype: Real = Real.half):
+        super().__init__(num_inputs, num_outputs, dtype)
+
+        self.parameters: Optional[Tensor] = None
+
+    def initialize(self, device: Device):
+        if "coopvec" not in device.features:
+            raise RuntimeError("Device does not support CoopVec")
+
+        fan_in = self.num_inputs
+        fan_out = self.num_outputs
+        cur_offset = 0
+        elem_size = self.dtype.size()
+
+        cur_offset = device.coopvec_align_matrix_offset(cur_offset)
+        weight_offset = cur_offset // elem_size
+        desc = device.coopvec_create_matrix_desc(
+            fan_out, fan_in, CoopVecMatrixLayout.training_optimal, self.dtype.sgl(), cur_offset)
+        cur_offset += desc.size
+        weight_count = desc.size // elem_size
+
+        cur_offset = device.coopvec_align_vector_offset(cur_offset)
+        bias_offset = cur_offset // elem_size
+        cur_offset += fan_out * elem_size
+        bias_count = fan_out
+
+        param_count = cur_offset // elem_size
+        self.parameters = Tensor.empty(device, (param_count, ), str(self.dtype))
+        self.offsets = [weight_offset, bias_offset]
+
+        # Xavier uniform initialization
+        std = math.sqrt(2.0 / (fan_in + fan_out))
+        a = math.sqrt(3.0) * std
+        weights_np = np.random.uniform(-a, a, (fan_out, fan_in)).astype(self.dtype.numpy())
+        biases_np = np.zeros((fan_out, ), dtype=self.dtype.numpy())
+
+        params_np = np.zeros((param_count, ), dtype=self.dtype.numpy())
+        coopvec_weights_np = params_np[weight_offset:weight_offset + weight_count]
+        coopvec_biases_np = params_np[bias_offset:bias_offset + bias_count]
+
+        device.coopvec_convert_matrix_host(
+            weights_np, coopvec_weights_np, dst_layout=CoopVecMatrixLayout.training_optimal)
+        coopvec_biases_np[:] = biases_np[:]
+
+        self.parameters.storage.copy_from_numpy(params_np)
+
+    def get_this(self):
+        if self.parameters is None:
+            raise RuntimeError("LinearLayer is not initialized!")
+
+        return {
+            "parameters": self.parameters.storage,
+            "gradients": self.parameters.grad_out.storage,
+            "offsets": self.offsets,
+            "_type": f"CoopVecLinearLayer<{self.dtype}, {self.num_inputs}, {self.num_outputs}> "
+        }
+
+
+# Root class for all coopvec activations (i.e. that implement IActivation)
+class Activation(CoopVecModule):
+    def __init__(self, act_name: str, width: int, dtype: Real = Real.float):
+        super().__init__(width, width, dtype)
+        self.act_name = act_name
+
+    def get_this(self):
+        return {"_type": f"CoopVecAct::{self.act_name}<{self.dtype}, {self.num_inputs}>"}
+
+
+class NoneAct(Activation):
+    def __init__(self, width: int, dtype: Real = Real.float):
+        super().__init__("None", width, dtype)
+
+
+class ReLUAct(Activation):
+    def __init__(self, width: int, dtype: Real = Real.float):
+        super().__init__("ReLU", width, dtype)
+
+
+class LeakyReLUAct(Activation):
+    def __init__(self, width: int, negative_slope: float = 0.01, dtype: Real = Real.float):
+        super().__init__("LeakyReLU", width, dtype)
+        self.negative_slope = negative_slope
+
+    def get_this(self):
+        return super().get_this() | {"negativeSlope": self.negative_slope}
+
+
+class ELUAct(Activation):
+    def __init__(self, width: int, a: float = 1.0, dtype: Real = Real.float):
+        super().__init__("ELU", width, dtype)
+        self.a = a
+
+    def get_this(self):
+        return super().get_this() | {"a": self.a}
+
+
+class SwishAct(Activation):
+    def __init__(self, width: int, dtype: Real = Real.float):
+        super().__init__("Swish", width, dtype)
+
+
+class TanhAct(Activation):
+    def __init__(self, width: int, dtype: Real = Real.float):
+        super().__init__("Tanh", width, dtype)
+
+
+class SigmoidAct(Activation):
+    def __init__(self, width: int, dtype: Real = Real.float):
+        super().__init__("Sigmoid", width, dtype)
+
+
+class ExpAct(Activation):
+    def __init__(self, width: int, dtype: Real = Real.float):
+        super().__init__("Exp", width, dtype)
--- experiments/neuralnetwork/network/NeuralModules.py
+++ experiments/neuralnetwork/network/NeuralModules.py
@@ -1,43 +1,33 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-#
-# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto. Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
 from __future__ import annotations
 
-from slangpy.backend import Device, DataType
+from slangpy.backend import Device, DataType, TypeReflection
 from slangpy.types import Tensor, NDBuffer
 from typing import Optional, Any
+from enum import Enum
 import numpy as np
 import math
 
 
 # Root interface representing a slang type that implements the IModule interface
 class NeuralModule:
-    def __init__(self, fan_in: int, fan_out: int, dtype: DataType = DataType.float32):
-        super().__init__()
+    @property
+    def input_type(self) -> str:
+        raise NotImplementedError()
 
-        self.dtype = dtype
-        self.fan_in = fan_in
-        self.fan_out = fan_out
+    @property
+    def output_type(self) -> str:
+        raise NotImplementedError()
 
     # Returns a dictionary containing the data for the slang struct
     def get_this(self) -> dict[str, Any]:
         raise NotImplementedError()
 
-    @property
-    def type_name(self):
-        return self.get_this()["_type"]
-
     # Returns a list of this module and all child modules
     def modules(self) -> list[NeuralModule]:
         return [self]
 
-    def initialize(self, device):
+    def initialize(self, device: Device):
         pass
 
     def parameters(self) -> list[Tensor]:
@@ -47,14 +37,22 @@ class NeuralModule:
 # Chains multiple modules together into a new module
 class ModuleChain(NeuralModule):
     def __init__(self, *modules: NeuralModule):
+        super().__init__()
+
         if len(modules) == 0:
             raise ValueError("Module chain needs at least one module")
 
         self.chain = list(modules)
 
-        super().__init__(modules[0].fan_in, modules[-1].fan_out, modules[0].dtype)
+    @property
+    def input_type(self) -> str:
+        return self.chain[0].input_type
 
-    def initialize(self, device):
+    @property
+    def output_type(self) -> str:
+        return self.chain[-1].output_type
+
+    def initialize(self, device: Device):
         for m in self.chain:
             m.initialize(device)
 
@@ -62,29 +60,95 @@ class ModuleChain(NeuralModule):
         return sum((m.parameters() for m in self.chain), start=[])
 
     def get_this(self):
-        result = self.chain[-1].get_this()
+        second_data = self.chain[-1].get_this()
 
-        for m in reversed(self.chain[:-1]):
-            first = m.get_this()
-            type_name = ("ModuleChain<"
-                         f"{dtype_name(self.dtype)}, "
-                         f"{m.fan_in}, {m.fan_out}, {self.fan_out}, "
-                         f"{first['_type']}, {result['_type']}> ")
-            result = {
+        for first in reversed(self.chain[:-1]):
+            first_data = first.get_this()
+            type_name = ("ChainedModulePair<"
+                         f"{first.input_type}, {first.output_type}, {self.output_type}, "
+                         f"{first_data['_type']}, {second_data['_type']}>")
+
+            second_data = {
                 "_type": type_name,
-                "first": first,
-                "second": result
+                "first": first_data,
+                "second": second_data
             }
-        return result
+        return second_data
 
     def modules(self) -> list[NeuralModule]:
         return sum((m.modules() for m in self.chain), start=[self])
 
 
-# Frequency encoding that maps each input parameter into a series
-# of sines and cosines with increasing frequency
-class FrequencyEncoding(NeuralModule):
-    def __init__(self, input_width: int, num_octaves: int, dtype: DataType = DataType.float32):
+class Real(Enum):
+    half = 1
+    float = 2
+    double = 3
+
+    def __str__(self):
+        return self._name_
+
+    def numpy(self):
+        if self is Real.half:
+            return np.float16
+        elif self is Real.float:
+            return np.float32
+        elif self is Real.double:
+            return np.float64
+        else:
+            raise ValueError(f"Invalid Real type '{self}'")
+
+    def slang(self):
+        if self is Real.half:
+            return TypeReflection.ScalarType.float16
+        elif self is Real.float:
+            return TypeReflection.ScalarType.float32
+        elif self is Real.double:
+            return TypeReflection.ScalarType.float64
+        else:
+            raise ValueError(f"Invalid Real type '{self}'")
+
+    def sgl(self):
+        if self is Real.half:
+            return DataType.float16
+        elif self is Real.float:
+            return DataType.float32
+        elif self is Real.double:
+            return DataType.float64
+        else:
+            raise ValueError(f"Invalid Real type '{self}'")
+
+    def size(self):
+        if self is Real.half:
+            return 2
+        elif self is Real.float:
+            return 4
+        elif self is Real.double:
+            return 8
+        else:
+            raise ValueError(f"Invalid Real type '{self}'")
+
+
+class ArrayModule(NeuralModule):
+    def __init__(self, num_inputs: int, num_outputs: int, dtype: Real):
+        super().__init__()
+
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.dtype = dtype
+
+    @property
+    def input_type(self) -> str:
+        return f"{self.dtype}[{self.num_inputs}]"
+
+    @property
+    def output_type(self) -> str:
+        return f"{self.dtype}[{self.num_outputs}]"
+
+
+class FrequencyEncoding(ArrayModule):
+    # Frequency encoding that maps each input parameter into a series
+    # of sines and cosines with increasing frequency
+    def __init__(self, input_width: int, num_octaves: int, dtype: Real = Real.float):
         if num_octaves == 0:
             raise ValueError("Missing argument: num_octaves")
 
@@ -94,31 +158,31 @@ class FrequencyEncoding(NeuralModule):
         super().__init__(input_width, output_width, dtype)
 
     def get_this(self):
-        return {"_type": f"FrequencyEncoding<{dtype_name(self.dtype)}, {self.fan_in}, {self.num_octaves}> "}
+        return {"_type": f"FrequencyEncoding<{self.dtype}, {self.num_inputs}, {self.num_octaves}>"}
 
 
 # Root class for all activations (i.e. that implement IActivation)
-class Activation(NeuralModule):
-    def __init__(self, act_name: str, width: int, dtype: DataType = DataType.float32):
+class Activation(ArrayModule):
+    def __init__(self, act_name: str, width: int, dtype: Real = Real.float):
         super().__init__(width, width, dtype)
         self.act_name = act_name
 
     def get_this(self) -> dict[str, Any]:
-        return {"_type": f"Activation::{self.act_name}<{dtype_name(self.dtype)}, {self.fan_in}> "}
+        return {"_type": f"Activation::{self.act_name}<{self.dtype}, {self.num_inputs}>"}
 
 
 class NoneAct(Activation):
-    def __init__(self, width: int, dtype: DataType = DataType.float32):
+    def __init__(self, width: int, dtype: Real = Real.float):
         super().__init__("None", width, dtype)
 
 
 class ReLUAct(Activation):
-    def __init__(self, width: int, dtype: DataType = DataType.float32):
+    def __init__(self, width: int, dtype: Real = Real.float):
         super().__init__("ReLU", width, dtype)
 
 
 class LeakyReLUAct(Activation):
-    def __init__(self, width: int, negative_slope: float = 0.01, dtype: DataType = DataType.float32):
+    def __init__(self, width: int, negative_slope: float = 0.01, dtype: Real = Real.float):
         super().__init__("LeakyReLU", width, dtype)
         self.negative_slope = negative_slope
 
@@ -127,7 +191,7 @@ class LeakyReLUAct(Activation):
 
 
 class ELUAct(Activation):
-    def __init__(self, width: int, a: float = 1.0, dtype: DataType = DataType.float32):
+    def __init__(self, width: int, a: float = 1.0, dtype: Real = Real.float):
         super().__init__("ELU", width, dtype)
         self.a = a
 
@@ -136,44 +200,42 @@ class ELUAct(Activation):
 
 
 class SwishAct(Activation):
-    def __init__(self, width: int, dtype: DataType = DataType.float32):
+    def __init__(self, width: int, dtype: Real = Real.float):
         super().__init__("Swish", width, dtype)
 
 
 class TanhAct(Activation):
-    def __init__(self, width: int, dtype: DataType = DataType.float32):
+    def __init__(self, width: int, dtype: Real = Real.float):
         super().__init__("Tanh", width, dtype)
 
 
 class SigmoidAct(Activation):
-    def __init__(self, width: int, dtype: DataType = DataType.float32):
+    def __init__(self, width: int, dtype: Real = Real.float):
         super().__init__("Sigmoid", width, dtype)
 
 
 class ExpAct(Activation):
-    def __init__(self, width: int, dtype: DataType = DataType.float32):
+    def __init__(self, width: int, dtype: Real = Real.float):
         super().__init__("Exp", width, dtype)
 
 
-class LinearLayer(NeuralModule):
-    def __init__(self, fan_in: int, fan_out: int, dtype: DataType = DataType.float32):
-        super().__init__(fan_in, fan_out, dtype)
+class LinearLayer(ArrayModule):
+    def __init__(self, num_inputs: int, num_outputs: int, dtype: Real = Real.float):
+        super().__init__(num_inputs, num_outputs, dtype)
 
         self.weights: Optional[Tensor] = None
         self.biases: Optional[Tensor] = None
 
     def initialize(self, device: Device):
         # Xavier uniform initialization
-        std = math.sqrt(2.0 / (self.fan_in + self.fan_out))
+        std = math.sqrt(2.0 / (self.num_inputs + self.num_outputs))
         a = math.sqrt(3.0) * std
-        weights_np = np.random.uniform(-a, a, (self.fan_out, self.fan_in)
-                                       ).astype(dtype_to_numpy(self.dtype))
-        biases_np = np.zeros((self.fan_out, ), dtype=dtype_to_numpy(self.dtype))
+        weights_np = np.random.uniform(-a, a, (self.num_outputs,
+                                       self.num_inputs)).astype(self.dtype.numpy())
+        biases_np = np.zeros((self.num_outputs, ), dtype=self.dtype.numpy())
 
-        self.weights = Tensor.empty(device, weights_np.shape,
-                                    dtype_name(self.dtype)).with_grads(zero=True)
-        self.biases = Tensor.empty(device, biases_np.shape,
-                                   dtype_name(self.dtype)).with_grads(zero=True)
+        self.weights = Tensor.empty(device, weights_np.shape, str(self.dtype)).with_grads(zero=True)
+        self.biases = Tensor.empty(device, biases_np.shape, str(self.dtype)).with_grads(zero=True)
         self.weights.storage.copy_from_numpy(weights_np)
         self.biases.storage.copy_from_numpy(biases_np)
 
@@ -189,23 +251,5 @@ class LinearLayer(NeuralModule):
             "biases": self.biases.storage,
             "weightGrads": self.weights.grad_out.storage,
             "biasGrads": self.biases.grad_out.storage,
-            "_type": f"LinearLayer<{dtype_name(self.dtype)}, {self.fan_in}, {self.fan_out}> "
+            "_type": f"LinearLayer<{self.dtype}, {self.num_inputs}, {self.num_outputs}>"
         }
-
-
-def dtype_name(dtype: DataType):
-    if dtype == DataType.float16:
-        return "half"
-    elif dtype == DataType.float32:
-        return "float"
-    else:
-        raise ValueError(f"Unsupported CoopVec datatype '{dtype}'")
-
-
-def dtype_to_numpy(dtype: DataType):
-    if dtype == DataType.float16:
-        return np.float32
-    elif dtype == DataType.float32:
-        return np.float32
-    else:
-        raise ValueError(f"Unsupported CoopVec datatype '{dtype}'")
