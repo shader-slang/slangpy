6c31943c8b26b2247ca152807fd1f16cf051d81f
Chris Cummings <ccummings@nvidia.com>
2025-02-27 09:27:08 +0000

    Test for generic resolution bug

--- slangpy/tests/test_generics.py
+++ slangpy/tests/test_generics.py
@@ -66,5 +66,69 @@ def test_generic_matrix(device_type: DeviceType, container_type: str, dim: int):
                     "<let N: int>", f'float{dim}x{dim}')
 
 
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+@pytest.mark.parametrize("explicit", [True, False])
+def test_arithmetic_generic_arguments_bug(device_type: DeviceType, explicit: bool):
+
+    # This test reproduces a Slang issue: https://github.com/shader-slang/slang/issues/6463
+    # Attempting to resolve a generic with arithmetic arguments fails. When the test is
+    # run with 'explicit' specialization, we generate and succesfully find the function.
+    # When we rely on Slang to solve the generic, it currently fails.
+
+    CODE = """
+interface IModule<int N, int M>
+{
+    float[M] eval(float[N] x);
+}
+
+struct LinearLayer<int N, int M> : IModule<N, M>
+{
+    float[M] eval(float[N] x)
+    {
+        float[M] result;
+        [ForceUnroll]
+        for (int i = 0; i < M; ++i) result[i] = 0.f;
+        return result;
+    }
+};
+
+float trainMaterial<
+    int numLatents,
+    Encoder : IModule<12, numLatents>,
+    Decoder : IModule<numLatents+6, 3> // <---- here!
+>
+(Encoder encoder, Decoder decoder)
+{
+    return 0.f;
+}
+"""
+
+    device = helpers.get_device(device_type)
+    module = helpers.create_module(device, CODE)
+
+    class LinearLayer():
+        def __init__(self, n: int = 0, m: int = 0):
+            super().__init__()
+            self.n = n
+            self.m = m
+
+        def get_this(self):
+            return {
+                "_type": f"LinearLayer<{self.n}, {self.m}> "
+            }
+
+    encoder = LinearLayer(12, 8)
+    decoder = LinearLayer(8+6, 3)
+
+    if explicit:
+        func_name = f"trainMaterial<{encoder.m}, {encoder.get_this()['_type']}, {decoder.get_this()['_type']}>"
+        func = module.find_function(func_name)
+        assert func is not None
+        func(encoder, decoder)
+    else:
+        pytest.skip("Fails due to https://github.com/shader-slang/slang/issues/6463")
+        module.trainMaterial(encoder, decoder)
+
+
 if __name__ == "__main__":
     pytest.main([__file__, "-v", "-s"])
