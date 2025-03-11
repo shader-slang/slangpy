63c128402cb55f95500de7e8e66e6ce9de774b40
Benedikt Bitterli <bbitterli@nvidia.com>
2025-03-11 00:30:03 -0700

    Update module __init__

--- experiments/neuralnetwork/neuralnetworks/__init__.py
+++ experiments/neuralnetwork/neuralnetworks/__init__.py
@@ -1,9 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # pyright: reportUnusedImport=false
 
-from basetypes import IModel, Real, RealArray, ArrayKind, Auto, AutoSettable
-from components import ModelChain, Convert
-from components import Activation, Identity, ReLU, LeakyReLU, ELU, Swish, Tanh, Sigmoid, Exp
-from components import LinearLayer, FrequencyEncoding
-from optimizers import Optimizer, AdamOptimizer, FullPrecisionOptimizer
+from .basetypes import *
+from .components import *
+from .optimizers import *
 from .utils import slang_include_paths
--- experiments/neuralnetwork/neuralnetworks/basetypes/__init__.py
+++ experiments/neuralnetwork/neuralnetworks/basetypes/__init__.py
@@ -1,9 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # pyright: reportUnusedImport=false
 
-from .TypeLike import TypeLike
-from .Auto import Auto, AutoSettable
+from .Auto import Auto, AutoSettable, resolve_auto
 from .CoopVecType import CoopVecType
-from .IModel import IModel, ModelError
+from .IModel import IModel, ModelError, SlangType
 from .Real import Real
 from .RealArray import RealArray, ArrayKind
