a8b5758575245482e3474723dc8744c28493e8a6
Benedikt Bitterli <bbitterli@nvidia.com>
2025-03-11 00:29:49 -0700

    Remove obsolete TypeLike

--- experiments/neuralnetwork/neuralnetworks/basetypes/TypeLike.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-from typing import Union
-from slangpy import Struct
-from slangpy.reflection import SlangType
-
-TypeLike = Union[SlangType, Struct]
