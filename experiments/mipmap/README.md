fe336a901d1f7707d3b7468e92d0871228b73807
Jesse Archer <jarcher@nvidia.com>
2025-05-07 23:46:13 -0700

    Render difference in loss values
    Render the difference in loss values between the trained inputs, and the
    downsamples inputs to show which is better.

--- experiments/mipmap/README.md
+++ experiments/mipmap/README.md
@@ -6,25 +6,22 @@ python main.py
 The example requires slangpy:
 pip install slangpy
 
-The purpose of this example is to render pixels using input material and
-normal map textures, and calculate per-pixel lighting.
+The purpose of this example is to render BRDF using input material and
+normal map textures.
 
 This can be expected to give correct results at full res, but when using
 lower res texture inputs (mipmap levels) the result will be somewhat
 incorrect, represented as per-pixel L2 loss values, which tell us how
 the inputs need to change to give the correct values.
 
-This app has 5 modes, set by the mode global variable in main.py:
-1. Evaluate pixel color using full res textures. This simply reads full res
-   texture data and evaluates a simple BRDF (in this case phone).
-2. Evaluate a lower res pixel color by using mode (1) to evaluate 4 adjacent
-   values and take the average. This is similar to using lower res mipmap
-   levels but should give a more accurate result.
-3. Evaluate a lower res pixel by using mode (1) with lower res texture
-   inputs, ie, lower res mipmap levels. This should be less accurate
-   than mode 2.
-4. Show the difference between mode (1) and (3), ie the per-pixel
-   L2 loss values between full res and low res inputs.
-5. Train per-pixel loss values to reach the expected values from (4).
-   This is shown by rendering the learned loss values as white pixels,
-   which should match the values from mode (4).
+This example trains the normal and roughness maps so that the rendered
+output BRDF looks as closs as possible to the downsampled original.
+
+The rendered image shows four outputs:
+1. The current renderered BRDF with a random light and view direction,
+   using the downsampled albedo, and trained normal and roughness.
+2. The difference between the L2 loss values of the trained result
+   vs using the lower res texture inputs. Green pixels indicate a
+   better result and red pixels indicate a worse result.
+3. The trained normal map.
+4. The trained roughness map.
--- experiments/mipmap/main.py
+++ experiments/mipmap/main.py
@@ -21,8 +21,8 @@ module = spy.Module.load_from_file(app.device, "mipmapping.slang")
 # incorrect, represented as per-pixel L2 loss values, which tell us how
 # the inputs need to change to give the correct values.
 #
-# This example trains the mipmaps so that the rendered output looks as close
-# as possible to the downsampled original
+# This example trains the normal and roughness maps so that the rendered
+# output BRDF looks as closs as possible to the downsampled original.
 
 
 # Generate a UV grid for the window, starting at (0, 0) for the top left pixel
@@ -83,17 +83,18 @@ downsampled_normal_map = downsampleTensorFloat3(normal_map)
 downsampled_roughness_map = downsampleTensorFloat(roughness_map)
 
 
-# Tensor for training the normal map.
-# One option is to start from the downsampled normal map.
-#trained_normals_without_grads: spy.Tensor = downsampleTensor(normal_map)
+# Tensors for training the normals and roughness.
+# One option is to start from the downsampled normal and roughness maps.
+#trained_normals_without_grads: spy.Tensor = downsampleTensorFloat3(normal_map)
+#trained_roughness_without_grads: spy.Tensor = downsampleTensorFloat(roughness_map)
 
 # Another option is to start with uniform normals, however this only works with an Adam optimizer.
 trained_normals_without_grads = spy.Tensor.empty(app.device, shape=(512,512), dtype='float3')
 module.baseNormal(_result=trained_normals_without_grads)
-trained_normals = trained_normals_without_grads.with_grads()
-
 trained_roughness_without_grads = spy.Tensor.empty(app.device, shape=(512,512), dtype='float')
 module.baseRoughness(_result=trained_roughness_without_grads)
+
+trained_normals = trained_normals_without_grads.with_grads()
 trained_roughness = trained_roughness_without_grads.with_grads()
 
 # Tensor containing the training loss and its derivative to propagate backwards (set to 1).
@@ -107,60 +108,86 @@ v_normal = spy.Tensor.zeros(module.device, trained_normals.shape, module.float3)
 m_roughness = spy.Tensor.zeros(module.device, trained_roughness.shape, module.float)
 v_roughness = spy.Tensor.zeros(module.device, trained_roughness.shape, module.float)
 
-# This learning rate seems to produce a reasonable result using gradient descent.
-grad_learning_rate = 0.001
-# This learning rate seems better for the adam optimizer.
-adam_learning_rate = 0.1
+# Learning rate for training.
+learning_rate = 0.001
+
+
+# In case we want to start training when pressing the 'tab' key.
+#start = False
+#
+#def on_keyboard_event(key: sgl.KeyboardEvent):
+#    if key.type == sgl.KeyboardEventType.key_press and key.key == sgl.KeyCode.tab:
+#        global start
+#        start = True
+#
+#app.on_keyboard_event = on_keyboard_event
+
 
 # Run the training.
 iter = 0
 while app.process_events():
-    # Generate random light and view dirs on the hemisphere
+    #if not start:
+    #    continue
+    # Generate random light and view dirs on the hemisphere.
     light_dir = getRandomDir()
     view_dir = getRandomDir()
 
-    # Alternative: Smooth sweep of light direction
+    # Alternative: Smooth sweep of light direction.
     #t = math.sin(time.time()*2)*1
     #light_dir = sgl.math.normalize(sgl.float3(t, t, 1.0))
+    #view_dir = sgl.float3(0, 0, 1)
 
-    # Full res rendered output BRDF from full res inputs (mode 1).
-    rendered: spy.Tensor = module.renderFullRes(albedo_map, normal_map, roughness_map, light_dir, view_dir, _result='tensor')
+    # Full res rendered output BRDF from full res inputs.
+    rendered: spy.Tensor = module.renderFullRes(albedo_map, normal_map,
+        roughness_map, light_dir, view_dir, _result='tensor')
 
-    # Downsampled output (avg) from the full res inputs (mode 2).
+    # Downsampled output (avg) from the full res inputs.
     downsampled = downsampleTensorFloat3(rendered)
+
+    # Lower res BRDF from downsampled inputs (for comparison).
+    lowres: spy.Tensor = module.renderFullRes(downsampled_albedo_map, downsampled_normal_map,
+        downsampled_roughness_map, light_dir, view_dir, _result='tensor')
 
     # Take the function that calculates the loss, i.e. the difference between the downsampled output
     # and the output calculated with downsampled albedo/normals, and run it 'backwards'
     # This propagates the gradient of training_loss back to the gradients of trained_normals.
     module.calculateLoss.bwds(downsampled, downsampled_albedo_map,
-                              trained_normals, trained_roughness, light_dir, view_dir, _result=training_loss)
+        trained_normals, trained_roughness, light_dir, view_dir, _result=training_loss)
     # trained_normals.grad_out now tells us how trained_normals needs to change
     # so that training_loss changes by training_loss.grad_in
 
     # We want training_loss to go down, so we subtract a tiny bit of that gradient.
-    #module.gradientDescent(trained_normals, trained_normals.grad, grad_learning_rate)
-    # In the next iteration, the updated trained_normals now hopefully reduces the loss
+    # One option is to use gradient descent which is simple but can get stuck in local minima.
+    #module.gradientDescentFloat3(trained_normals, trained_normals.grad, learning_rate)
+    #module.gradientDescentFloat3(trained_roughness, trained_roughness.grad, learning_rate)
+    # In the next iteration, the updated trained_normals now hopefully reduces the loss.
 
     # Another option is to use an Adam optimizer.
-    module.adamFloat3(trained_normals, trained_normals.grad, m_normal, v_normal, adam_learning_rate)
-    module.adamFloat(trained_roughness, trained_roughness.grad, m_roughness, v_roughness, adam_learning_rate)
+    module.adamFloat3(trained_normals, trained_normals.grad, m_normal, v_normal, learning_rate)
+    module.adamFloat(trained_roughness, trained_roughness.grad, m_roughness, v_roughness, learning_rate)
 
-    iter += 1
+    # This is just here for debugging purposes, to confirm the data looks correct.
+    #iter += 1
+    #if iter % 50 == 0:
+    #    iter = 0
+    #    resultArray = training_loss.to_numpy()
+    #    loss = np.sum(resultArray) / resultArray.size
+    #    print("Iteration: {}, Loss: {}".format(iter, loss))
+    #    print("parameter {}".format(trained_normals.to_numpy()))
 
-    if iter % 50 == 0:
-        resultArray = training_loss.to_numpy()
-        loss = np.sum(resultArray) / resultArray.size
-        print("Iteration: {}, Loss: {}".format(iter, loss))
-        print("parameter {}".format(trained_normals.to_numpy()))
-
-    # Render current progress
+    # Render current progress.
     loss: spy.Tensor = module.renderLoss(
         downsampled, downsampled_albedo_map, trained_normals, trained_roughness, light_dir, view_dir, _result='tensor')
     result: spy.Tensor = module.renderFullRes(
         downsampled_albedo_map, trained_normals, trained_roughness, light_dir, view_dir, _result='tensor')
 
-    # TODO: We could also throw in some more visualization modes.
-    module.showTrainingProgress(result, loss, downsampled_normal_map,
-                                trained_normals_without_grads, trained_roughness_without_grads, windowUVs, _result=app.output)
+    # Compare the loss between the trained result and the lowres result.
+    # Green pixels indicate a better result, and red pixels indicate a worse
+    # result.
+    loss_lowres: spy.Tensor = module.lossLowres(downsampled, lowres, _result='tensor')
+    loss_diff: spy.Tensor = module.lossDiff(loss, loss_lowres, _result='tensor')
+
+    module.showTrainingProgress(result, loss_diff, downsampled_normal_map,
+        trained_normals_without_grads, trained_roughness_without_grads, windowUVs, _result=app.output)
 
     app.present()
