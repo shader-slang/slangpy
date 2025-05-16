5b0aa57b213f723b08721963c0413e4adc198222
Chris Cummings <ccummings@nvidia.com>
2025-05-16 16:03:13 +0100

    Formatting

--- slangpy/builtin/ndbuffer.py
+++ slangpy/builtin/ndbuffer.py
@@ -408,7 +408,7 @@ class NDDifferentiableBufferMarshall(BaseNDBufferMarshall):
                                 for i in range(len(ndbuffer.strides))
                             ],
                             "offset": ndbuffer.offset,
-                        }
+                        },
                     }
             return res
 
