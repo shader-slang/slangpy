5b908cfc22269bcc06943839d9b3d2d41e1bbac1
Chris Cummings <ccummings@nvidia.com>
2025-06-02 11:20:18 +0100

    Work on building shader objects

---
+++ slangpy/core/finalisedarg.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+from typing import Any, cast
+from slangpy import Module, ShaderCursor
+from slangpy.core.native import unpack_arg, get_value_signature, NativeMarshall, CallMode
+from slangpy.bindings import get_or_create_type, PYTHON_TYPES, Marshall, BindContext
+from slangpy.reflection import SlangType, SlangProgramLayout
+
+
+class FinalisedArg:
+    def __init__(self, module: Module, arg_value: Any):
+        super().__init__()
+
+        self.python = get_or_create_type(module.layout, type(arg_value), arg_value)
+        self.slangpy_signature = get_value_signature(arg_value)
+        self.value = self.python.build_shader_object(
+            BindContext(module.layout, CallMode.prim, module.device_module, {}), arg_value
+        )
+        if self.value is None:
+            raise ValueError(
+                f"Cannot build shader object for {arg_value} of type {type(arg_value)}"
+            )
+
+    def uniforms(self):
+        return self.value
