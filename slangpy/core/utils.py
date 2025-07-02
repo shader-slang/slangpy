beceda36bf89df949c377a133f773a879819eca7
Benedikt Bitterli <bbitterli@nvidia.com>
2025-07-02 00:38:10 -0700

    Expose bindless options in spy.create_device

--- slangpy/core/utils.py
+++ slangpy/core/utils.py
@@ -11,6 +11,7 @@ from slangpy import (
     DeviceType,
     Device,
     NativeHandle,
+    BindlessDesc,
 )
 from slangpy.reflection import SlangType
 import builtins
@@ -27,6 +28,7 @@ def create_device(
     enable_hot_reload: bool = True,
     enable_compilation_reports: bool = False,
     existing_device_handles: Optional[Sequence[NativeHandle]] = None,
+    bindless_options: Optional[BindlessDesc] = None
 ):
     """
     Create a device with basic settings for SlangPy. For full control over device init,
@@ -60,6 +62,7 @@ def create_device(
         enable_hot_reload=enable_hot_reload,
         enable_compilation_reports=enable_compilation_reports,
         existing_device_handles=existing_device_handles,
+        bindless_options=bindless_options
     )
 
     if is_running_in_jupyter():
