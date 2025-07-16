a02b6540fd8a7714ffae6533dec5237e11627261
Tomas Davidovic <tdavidovic@nvidia.com>
2025-07-16 17:23:41 +0200

    Small change to casting

--- src/slangpy_ext/nanobind.h
+++ src/slangpy_ext/nanobind.h
@@ -445,15 +445,14 @@ inline std::optional<DataStruct::Type> dtype_to_data_struct_type(nb::dlpack::dty
 namespace sgl::detail {
 
 template<typename TLen>
-inline Py_ssize_t sanitize_getitem_index(Py_ssize_t index_, TLen len_)
+inline Py_ssize_t sanitize_getitem_index(Py_ssize_t index, TLen len_)
 {
-    int64_t len = int64_t(len_);
-    int64_t index = int64_t(index_);
+    Py_ssize_t len = Py_ssize_t(len_);
     // throwing index_error allows this to be used as a python iterator
     if (index >= len || index < len)
         throw nb::index_error();
 
-    return Py_ssize_t((index >= 0) ? index : (index + len));
+    return (index >= 0) ? index : (index + len);
 }
 
 inline constexpr uint64_t const_hash(std::string_view str)
