2afce4bfe60fac7a7940e9acb4f7a46a4238aacd
Chris Cummings <ccummings@nvidia.com>
2025-08-29 13:25:05 +0100

    Extra tests + fix null check

--- slangpy/benchmarks/test_benchmark_interop.py
+++ slangpy/benchmarks/test_benchmark_interop.py
@@ -20,18 +20,94 @@ float add_floats(float a, float b) {
 
 ADD_COMPUTE = """
 [shader("compute")]
-[numthreads(1, 1, 32)]
+[numthreads(32, 1, 1)]
 void compute_main(uint3 tid: SV_DispatchThreadID, StructuredBuffer<float> a, StructuredBuffer<float> b, RWStructuredBuffer<float> res) {
     res[tid.x] = a[tid.x] + b[tid.x];
 }
 """
 
+ADD_COMPUTE_4X = """
+[shader("compute")]
+[numthreads(32, 1, 1)]
+void compute_main(uint3 tid: SV_DispatchThreadID, StructuredBuffer<float> a, StructuredBuffer<float> b, RWStructuredBuffer<float> res) {
+    uint idx = tid.x*4;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+}
+"""
 
-@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
-def test_pytorch_tensor_addition_cpu(
-    device_type: spy.DeviceType, benchmark_python_function: BenchmarkPythonFunction
-):
-    device = helpers.get_torch_device(device_type)
+ADD_COMPUTE_16X = """
+[shader("compute")]
+[numthreads(32, 1, 1)]
+void compute_main(uint3 tid: SV_DispatchThreadID, StructuredBuffer<float> a, StructuredBuffer<float> b, RWStructuredBuffer<float> res) {
+    uint idx = tid.x*16;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+    idx++;
+    res[idx] = a[idx] + b[idx];
+}
+"""
+
+ADD_COMPUTE_16X_LOOP = """
+[shader("compute")]
+[numthreads(32, 1, 1)]
+void compute_main(uint3 tid: SV_DispatchThreadID, StructuredBuffer<float> a, StructuredBuffer<float> b, RWStructuredBuffer<float> res) {
+    uint base = tid.x*16;
+    for(uint i = 0; i < 16; i++) {
+        uint idx = base + i;
+        res[idx] = a[idx] + b[idx];
+    }
+}
+"""
+
+ADD_COMPUTE_16X_UNROLL = """
+[shader("compute")]
+[numthreads(32, 1, 1)]
+void compute_main(uint3 tid: SV_DispatchThreadID, StructuredBuffer<float> a, StructuredBuffer<float> b, RWStructuredBuffer<float> res) {
+    uint base = tid.x*16;
+    [ForceUnroll]
+    for(uint i = 0; i < 16; i++) {
+        uint idx = base + i;
+        res[idx] = a[idx] + b[idx];
+    }
+}
+"""
+
+
+def test_pytorch_tensor_addition_cpu(benchmark_python_function: BenchmarkPythonFunction):
+    device = helpers.get_torch_device(spy.DeviceType.cuda)
 
     BUFFER_SIZE = 10
     import torch
@@ -61,7 +137,7 @@ def test_compute_addition_noalloc_cpu(
     kernel = device.create_compute_kernel(program)
 
     def tensor_addition(a: spy.NDBuffer, b: spy.NDBuffer, res: spy.NDBuffer):
-        kernel.dispatch(spy.uint3(1, 1, BUFFER_SIZE), a=a.storage, b=b.storage, res=res.storage)
+        kernel.dispatch(spy.uint3(BUFFER_SIZE, 1, 1), a=a.storage, b=b.storage, res=res.storage)
 
     benchmark_python_function(device, tensor_addition, a=buffer0, b=buffer1, res=resbuffer)
 
@@ -120,9 +196,8 @@ def test_slangpy_tensor_addition_appendonly_cpu(
     benchmark_python_function(device, tensor_addition, a=buffer0, b=buffer1, res=resbuffer)
 
 
-@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
-def test_pytorch_tensor_addition_gpu_est(device_type: spy.DeviceType, report: ReportFixture):
-    device = helpers.get_torch_device(device_type)
+def test_pytorch_tensor_addition_gpu_est(report: ReportFixture):
+    device = helpers.get_torch_device(spy.DeviceType.cuda)
 
     BUFFER_SIZE = 65535 * 32
     import torch
@@ -168,7 +243,113 @@ def test_compute_addition_noalloc_gpu(
     benchmark_compute_kernel(
         device,
         kernel,
-        spy.uint3(1, 1, BUFFER_SIZE),
+        spy.uint3(BUFFER_SIZE, 1, 1),
+        a=buffer0.storage,
+        b=buffer1.storage,
+        res=resbuffer.storage,
+    )
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_compute_addition_4x_noalloc_gpu(
+    device_type: spy.DeviceType, benchmark_compute_kernel: BenchmarkComputeKernel
+):
+    device = helpers.get_torch_device(device_type)
+
+    BUFFER_SIZE = 65535 * 32
+    buffer0 = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+    buffer1 = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+    resbuffer = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+
+    module = device.load_module_from_source("test_compute_addition_noalloc_gpu_4x", ADD_COMPUTE_4X)
+    program = device.link_program([module], [module.entry_point("compute_main")])
+    kernel = device.create_compute_kernel(program)
+
+    benchmark_compute_kernel(
+        device,
+        kernel,
+        spy.uint3(int(BUFFER_SIZE / 4), 1, 1),
+        a=buffer0.storage,
+        b=buffer1.storage,
+        res=resbuffer.storage,
+    )
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_compute_addition_16x_noalloc_gpu(
+    device_type: spy.DeviceType, benchmark_compute_kernel: BenchmarkComputeKernel
+):
+    device = helpers.get_torch_device(device_type)
+
+    BUFFER_SIZE = 65535 * 32
+    buffer0 = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+    buffer1 = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+    resbuffer = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+
+    module = device.load_module_from_source(
+        "test_compute_addition_noalloc_gpu_16x", ADD_COMPUTE_16X
+    )
+    program = device.link_program([module], [module.entry_point("compute_main")])
+    kernel = device.create_compute_kernel(program)
+
+    benchmark_compute_kernel(
+        device,
+        kernel,
+        spy.uint3(int(BUFFER_SIZE / 16), 1, 1),
+        a=buffer0.storage,
+        b=buffer1.storage,
+        res=resbuffer.storage,
+    )
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_compute_addition_16xloop_noalloc_gpu(
+    device_type: spy.DeviceType, benchmark_compute_kernel: BenchmarkComputeKernel
+):
+    device = helpers.get_torch_device(device_type)
+
+    BUFFER_SIZE = 65535 * 32
+    buffer0 = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+    buffer1 = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+    resbuffer = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+
+    module = device.load_module_from_source(
+        "test_compute_addition_16xloop_noalloc_gpu", ADD_COMPUTE_16X_LOOP
+    )
+    program = device.link_program([module], [module.entry_point("compute_main")])
+    kernel = device.create_compute_kernel(program)
+
+    benchmark_compute_kernel(
+        device,
+        kernel,
+        spy.uint3(int(BUFFER_SIZE / 16), 1, 1),
+        a=buffer0.storage,
+        b=buffer1.storage,
+        res=resbuffer.storage,
+    )
+
+
+@pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
+def test_compute_addition_16xunroll_noalloc_gpu(
+    device_type: spy.DeviceType, benchmark_compute_kernel: BenchmarkComputeKernel
+):
+    device = helpers.get_torch_device(device_type)
+
+    BUFFER_SIZE = 65535 * 32
+    buffer0 = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+    buffer1 = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+    resbuffer = spy.NDBuffer.empty(device, shape=(BUFFER_SIZE,), dtype=float)
+
+    module = device.load_module_from_source(
+        "test_compute_addition_16xunroll_noalloc_gpu", ADD_COMPUTE_16X_UNROLL
+    )
+    program = device.link_program([module], [module.entry_point("compute_main")])
+    kernel = device.create_compute_kernel(program)
+
+    benchmark_compute_kernel(
+        device,
+        kernel,
+        spy.uint3(int(BUFFER_SIZE / 16), 1, 1),
         a=buffer0.storage,
         b=buffer1.storage,
         res=resbuffer.storage,
@@ -223,8 +404,8 @@ def test_slangpy_addition_alloc_interop_cpu(
     buffer0 = torch.randn((BUFFER_SIZE,), dtype=torch.float32, device="cuda")
     buffer1 = torch.randn((BUFFER_SIZE,), dtype=torch.float32, device="cuda")
 
-    def tensor_addition(a: spy.NDBuffer, b: spy.NDBuffer, res: spy.NDBuffer):
-        func(a, b, _result=res)
+    def tensor_addition(a: spy.NDBuffer, b: spy.NDBuffer):
+        func(a, b)
 
     benchmark_python_function(device, tensor_addition, a=buffer0, b=buffer1)
 
@@ -265,4 +446,4 @@ def test_slangpy_addition_noalloc_interop_gpu_est(
 
 if __name__ == "__main__":
     # input("Press Enter to run the CUDA tensor test...")
-    pytest.main([__file__ + "::test_slangpy_addition_noalloc_interop_gpu_est", "-v", "-s"])
+    pytest.main([__file__, "-v", "-s"])
