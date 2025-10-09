9ced37960498bf61ba7c431c69754916441ea095
Julius Ikkala <julius.ikkala@gmail.com>
2025-10-09 19:14:55 +0300

    Fix formatting

--- slangpy/tests/device/test_print.py
+++ slangpy/tests/device/test_print.py
@@ -9,8 +9,11 @@ from slangpy.testing import helpers
 # Metal test disabled until printing of float16 values in Metal has been fixed
 # in Slang: https://github.com/shader-slang/slangpy/issues/497
 PRINT_DEVICE_TYPES = [
-    x for x in helpers.DEFAULT_DEVICE_TYPES if x in [spy.DeviceType.d3d12, spy.DeviceType.vulkan, spy.DeviceType.cuda]
+    x
+    for x in helpers.DEFAULT_DEVICE_TYPES
+    if x in [spy.DeviceType.d3d12, spy.DeviceType.vulkan, spy.DeviceType.cuda]
 ]
+
 
 @pytest.mark.parametrize("device_type", PRINT_DEVICE_TYPES)
 def test_print(device_type: spy.DeviceType):
