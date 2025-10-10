fadfe26964560cacf5eee44f1e8614969065d2ba
Julius Ikkala <julius.ikkala@gmail.com>
2025-10-10 20:17:51 +0300

    Fix formatting again

--- slangpy/tests/device/test_print.py
+++ slangpy/tests/device/test_print.py
@@ -12,7 +12,9 @@ def test_print(device_type: spy.DeviceType):
     # Metal test disabled until printing of float16 values in Metal has been fixed
     # in Slang.
     if device_type == spy.DeviceType.metal:
-        pytest.skip("Skipped due to issue in Slang: https://github.com/shader-slang/slangpy/issues/497")
+        pytest.skip(
+            "Skipped due to issue in Slang: https://github.com/shader-slang/slangpy/issues/497"
+        )
 
     device = spy.Device(type=device_type, enable_print=True, label=f"print-{device_type.name}")
     helpers.dispatch_compute(
