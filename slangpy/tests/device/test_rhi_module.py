939c03253f8578f68207d7f131beeb6393863c09
Simon Kallweit <skallweit@nvidia.com>
2025-11-25 14:32:10 +0100

    Fix test name

--- slangpy/tests/device/test_rhi_module.py
+++ slangpy/tests/device/test_rhi_module.py
@@ -7,7 +7,7 @@ from slangpy.testing import helpers
 
 
 @pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
-def test_print(device_type: spy.DeviceType):
+def test_rhi_module(device_type: spy.DeviceType):
     device = helpers.get_device(device_type)
     device.load_module("test_rhi_module.slang")
 
