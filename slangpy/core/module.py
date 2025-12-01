8d5432491d1db34ed63d1e0b5131ae8f60cc3ad0
Tomas Davidovic <tdavidovic@nvidia.com>
2025-12-01 18:37:53 +0100

    Added a comment

--- slangpy/core/module.py
+++ slangpy/core/module.py
@@ -61,7 +61,7 @@ class Module:
         #: The slangpy device module.
         self.slangpy_device_module = device_module.session.load_module("slangpy")
 
-        # Extract linked modules
+        # Extract linked modules, dict.fromkeys is used to remove duplicates while preserving order.
         self.link = list(dict.fromkeys([x.module if isinstance(x, Module) else x for x in link]))
 
         #: Reflection / layout information for the module.
