933077114132d8024e635d6b7e9435bcac70ca03
Simon Kallweit <skallweit@nvidia.com>
2026-01-15 13:55:08 +0100

    Crashing test for testing

---
+++ slangpy/tests/core/test_crash.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import pytest
+import slangpy as spy
+
+
+def test_crash():
+    spy.crash()
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v"])
