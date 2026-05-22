f8b034b5e53039a88d1533314008cb2c5c12ae96
Fangjun Zhou <fangjun.zhou02@gmail.com>
2026-05-22 16:45:56 -0700

    Unskip resource array and tensor array tests
    As issue https://github.com/shader-slang/slang/issues/7606 has been
    resolved.

--- slangpy/tests/slangpy_tests/test_array.py
+++ slangpy/tests/slangpy_tests/test_array.py
@@ -173,10 +173,6 @@ int inc(Val val) {
 
 @pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
 def test_vectorize_struct_with_resource_array(device_type: DeviceType):
-    if device_type == DeviceType.metal:
-        # https://github.com/shader-slang/slang/issues/7606
-        pytest.skip("Crash in the slang compiler")
-
     device = helpers.get_device(device_type)
     function = helpers.create_function_from_module(
         device,
@@ -211,10 +207,6 @@ int inc(Val val) {
 
 @pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
 def test_vectorize_struct_with_tensor_array(device_type: DeviceType):
-    if device_type == DeviceType.metal:
-        # https://github.com/shader-slang/slang/issues/7606
-        pytest.skip("Crash in the slang compiler")
-
     device = helpers.get_device(device_type)
     function = helpers.create_function_from_module(
         device,
@@ -245,10 +237,6 @@ float inc(Val val) {
 
 @pytest.mark.parametrize("device_type", helpers.DEFAULT_DEVICE_TYPES)
 def test_2d_mapped_vectorize_struct_with_tensor_array(device_type: DeviceType):
-    if device_type == DeviceType.metal:
-        # https://github.com/shader-slang/slang/issues/7606
-        pytest.skip("Crash in the slang compiler")
-
     device = helpers.get_device(device_type)
     function = helpers.create_function_from_module(
         device,
