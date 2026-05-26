f151748db6d0b4d6dfad47fe5690f458fc5379cd
Chris Cummings <ccummings@nvidia.com>
2026-05-26 12:47:53 +0100

    Cleanup

--- slangpy/types/tensor.py
+++ slangpy/types/tensor.py
@@ -4,4 +4,5 @@
 # Compatibility shim for the old location of Tensor and TensorDesc.
 # These should now be imported from slangpy.Tensor
 from ..native_func import Tensor, TensorDesc
+
 __all__ = ["Tensor", "TensorDesc"]
